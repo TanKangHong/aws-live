cc1194e15b04df3f637487a53b68f24c0f1881fd
TanKangHong <101232669+TanKangHong@users.noreply.github.com>
2023-08-28 10:49:02 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cpxtulz96ixw.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tankanghong-employee"
+customregion = "us-east-1"
+
